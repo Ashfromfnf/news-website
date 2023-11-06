4e757f08d427b1a79da796f3612b67cd50657c92
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2023-11-06 17:26:29 +0100

    24 changed files :pensive:
    FUCKING 24... PAIN

--- py/articles.py
+++ py/articles.py
@@ -2,9 +2,7 @@
 # articles and then turns them into html files so i don't really need
 # to rewrite everything every time i change the source code
 #
-# PATH: py/articles.txt
-
-txtPath = r"py/articles.txt"
+# PATH: py/articles.txt 
 
 txt = open("py\input.txt", "r", encoding="utf8").read()
 
@@ -23,16 +21,26 @@ print(listfinish)
 html = open("py/articlehtml.txt", "r", encoding="utf8").read()
 print(html)
 
-for i, val in listfinish:
-    obj = listfinish[val]
-    objli = obj.split("69TITILE69")
+
+listcomplete = [x.split("69TITLE69") for x in listfinish]
+
+print(listcomplete)
+
+for x in listcomplete:
+
+    
+    print("length:", len(x))
+
+    if len(x) <= 2:
+        title = x[0]
+        article = x[1]
+        html = open("py/articlehtml.txt", "r", encoding="utf8").read().format(title, title, article)
+        print(html)
+
+        with open(("./articles/" + title.replace(" - ", "-").replace(",", "").replace("""'""", "").replace("?", "").replace(" ", "-") + ".html"), "w", encoding="utf8") as file:
+            file.write(html)
+    else:
+        print(x + "\n \n \n ^^^^^^^^^^^ \n TOO SHORT!!!!")
 
 
-    title = objli[0]
-    article = objli[1]
-
-    html = open("py/articlehtml.txt", "r", encoding="utf8").read()
-
-    print(html)
-
-input("heyy")
+input()
