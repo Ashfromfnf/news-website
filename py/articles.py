41a177d16865b7cf7dd41f40efd92e5fb05221d1
Ash <43928265+Ashfromfnf@users.noreply.github.com>
2024-02-25 00:17:52 +0100

    yes

--- py/articles.py
+++ py/articles.py
@@ -6,12 +6,12 @@
 
 txt = open("py\input.txt", "r", encoding="utf8").read()
 
-txtlist = txt.split("YOU'REBEAUTIFUL")
 
 print(txt)
-print(txtlist)
 
-finish = txt.replace(r"\n", "")
+txt = txt.replace(r"\n", "<br>")
+finish = txt.replace(r"\'", "'")
+finish = finish.replace("69AUTHOR69", "YOU'REBEAUTIFUL")
 listfinish = finish.split("YOU'REBEAUTIFUL")
 
 print("list finish:")
@@ -19,28 +19,44 @@ print(listfinish)
 
 
 html = open("py/articlehtml.html", "r", encoding="utf8").read()
-print(html)
+print("html")
 
 
 listcomplete = [x.split("69TITLE69") for x in listfinish]
 
-print(listcomplete)
+
+print("\n\n\n\n\n\n\n\n\n\n\n listcomplete: \n\n\n\n\\n\n\n\n\n\\n\n\n\n\n\n\n\n" + str(listcomplete))
 
 for x in listcomplete:
+
+
+    if len(x) == 1:
+        prevtitle = str(x[0])
+        
 
     
     print("length:", len(x))
 
-    if len(x) <= 2:
+    if len(x) == 2:
+        x.insert(0, prevtitle)
         title = x[0]
         article = x[1]
-        html = open("py/articlehtml.html", "r", encoding="utf8").read().format(title, title, article)
+
+        print("the title should be inserted, quality check for further information: " + str(x))
+
+    if len(x) == 3:
+        title = x[0]
+        article = x[2]
+        author = x[1]
+        html = open("py/articlehtml.html", "r", encoding="utf8").read().format(title, title, article, author)
         print(html)
 
-        with open(("./articles/" + title.replace(" - ", "-").replace(",", "").replace("""'""", "").replace("?", "").replace(" ", "-") + ".html"), "w", encoding="utf8") as file:
+        print(str(x))
+        with open(("./articles/" + title.replace(" - ", "-").replace(",", "").replace("""'""", "").replace("?", "").replace(" ", "-").replace(":", "") + ".html"), "w", encoding="utf8") as file:
             file.write(html)
-    else:
-        print(x + "\n \n \n ^^^^^^^^^^^ \n FORMATTING ISSUE!!!!")
+
+    elif len(x) > 3:
+        print(str(x) + "\n \n \n ^^^^^^^^^^^ \n HEAVY FUCKING FORMATTING ISSUE!!!!" + "LENGTH: " + str(len(x)))
 
 
 input()
